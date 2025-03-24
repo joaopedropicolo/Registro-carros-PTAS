e7cd05b3a08629ec94bd14568ca34b75a82df07d
joaopedropicolo <bigwon132@gmail.com>
2025-03-24 14:26:17 -0400

    Rotas, controllers e index.js Ajustados

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
-const app = require("express")();
+const express = require("express");
+const app = express();
+
+
+app.use(express.urlencoded({extended: true}))
+app.use(express.json());
+
+const veiculosRoutes = require("./routes/veiculosRoutes")
+app.use("/veiculos", veiculosRoutes);
 
 app.get("/", (req, res)=>{
     res.send("Receba");
 });
 
 let port = 8000;
-app.listen(port, () => {
+app.listen(port, (err) => {
+    if(err){
+        console.log("Erro: "+JSONstringify(err));
+    } else {
     console.log(`Servidor rodando em http://localhost:${port}`);
+    }
 });
---
+++ routes/veiculosRoutes.js
@@ -0,0 +1,8 @@
+const router = require("express").Router();
+const VeiculoController = require("../controllers/VeiculoController")
+
+router.get("/cadastro", VeiculoController.formCadastro)
+router.post("/cadastro", VeiculoController.cadastrar)
+router.get("/todos", VeiculoController.buscarTodos)
+
+module.exports = router;
