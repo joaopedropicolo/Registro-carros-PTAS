aa9fb923392e2e79a027c3203712adfd1a503622
joaopedropicolo <bigwon132@gmail.com>
2025-04-28 12:54:08 -0400

    readme update + polimentos

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-tudo mentira
+# Projeto Detran PTAS 2
+
+Projeto de PTAS 2 orientado pelo professor Lucas.
+
+## Tecnologias Utilizadas
+
+- [React](https://reactjs.org/) (ou qualquer outra tecnologia usada no projeto)
+- [Prisma](https://www.prisma.io/?via=start&gad_source=1&gclid=EAIaIQobChMIq9-jqJj7jAMV7BxECB0X1QCvEAAYASAAEgKjT_D_BwE)
+
+## Como Rodar o Projeto
+
+Siga os passos abaixo para rodar o projeto localmente:
+
+# npm i
+
+### 1. Baixe as dependências
+
+No terminal, navegue até a pasta do projeto e execute o comando abaixo para instalar as bibliotecas necessárias:
+
+```bash
+npm install
--- index.js
+++ index.js
@@ -2,25 +2,25 @@ const express = require("express");
 const path = require("path");
 const app = express();
 
-app.use(express.urlencoded({extended: true}))
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-const veiculosRoutes = require("./routes/veiculosRoutes")
+const veiculosRoutes = require("./routes/veiculosRoutes");
 app.use("/veiculos", veiculosRoutes);
 
 app.get("/cadastro", (req, res) => {
     res.sendFile(path.join(__dirname, "views", "formVeiculo.html"));
-  });
+});
 
-app.get("/", (req, res)=>{
+app.get("/", (req, res) => {
     res.send("Receba");
 });
 
-let port = 8000; //8000 Default Port
+let port = 8000; // 8000 Default Port
 app.listen(port, (err) => {
-    if(err){
-        console.log("Erro: "+JSONstringify(err));
+    if (err) {
+        console.log("Erro: " + JSON.stringify(err));
     } else {
-    console.log(`Servidor rodando em http://localhost:${port}`);
+        console.log(`Servidor rodando em http://localhost:${port}`);
     }
 });
