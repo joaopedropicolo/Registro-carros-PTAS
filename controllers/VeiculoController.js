635a4ac295233faa40d00a8cb9734e3133981579
joaopedropicolo <bigwon132@gmail.com>
2025-04-28 14:26:04 -0400

    Espaçamento

--- controllers/VeiculoController.js
+++ controllers/VeiculoController.js
@@ -2,12 +2,10 @@ const path = require("path");
 const { PrismaClient } = require("@prisma/client")
 const prisma = new PrismaClient()
 
-
 class VeiculoController{
     static formCadastro(req, res) {
         res.sendFile(path.join(__dirname, ".. ", "views", "formVeiculo.html"));
     }
-    
     static async cadastrar(req, res) {
         //res.json(req.body);
         const veiculo = await prisma.veiculo.create({
@@ -20,7 +18,6 @@ class VeiculoController{
         })
         res.send(`O veiculo foi cadastrado sob o ID: ${veiculo.id}, modelo: ${veiculo.modelo}, ano: ${veiculo.ano} e cor: ${veiculo.cor}.`)
     }
-
     static buscarTodos(req, res) {}
 }
 
